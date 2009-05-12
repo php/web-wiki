175ceed077a0d54401f520d035025bcf99503444
Lukas Smith <lsmith@php.net>
2009-05-12 15:52:21 +0000

    resync with prod and add new conf option to manage the list of admins

--- dokuwiki/conf/local.php
+++ dokuwiki/conf/local.php
@@ -6,23 +6,23 @@
  * Date: Thu, 06 Mar 2008 20:30:50 +0000
  */
 
-umask(0775);
 
+$conf['title'] = 'PHP Wiki';
+$conf['template'] = 'phpwiki';
 $conf['useacl'] = 1;
+$conf['authtype'] = 'phpcvs';
+$conf['auth']['phpcvs'] = array('admins' => array('lsmith', 'pajoye', 'philip', 'scottmac', 'bjori'));
 $conf['superuser'] = '@admin';
 $conf['manager'] = '@phpcvs';
-$conf['registernotify'] = 'smith@pooteeweet.org';
-$conf['spellchecker'] = 1;
+$conf['registernotify'] = 'php-webmaster@lists.php.net';
+$conf['updatecheck'] = 0;
 $conf['userewrite'] = '1';
 $conf['useslash'] = 1;
-$conf['mailfrom'] = 'php-webmaster@php.net';
+$conf['mailfrom'] = 'php-webmaster@lists.php.net';
 $conf['send404'] = 1;
-$conf['plugin']['hcalendar']['locale'] = 'en_US';
-$conf['updatecheck'] = 0;
 $conf['rss_content'] = 'htmldiff';
-$conf['title']       = 'PHP Wiki';
-$conf['template']    = 'phpwiki';
-$conf['authtype']    = 'phpcvs';
+$conf['plugin']['hcalendar']['locale'] = 'en_US';
+$conf['spellchecker'] = '1';
 
 @include(DOKU_CONF.'local.protected.php');
 

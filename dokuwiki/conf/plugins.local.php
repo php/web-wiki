0eca1908884c2ad0634d2eaf70866d42782bce9a
Hannes Magnusson <bjori@mongodb.com>
2013-12-16 10:32:52 -0800

    Disable all these extra plugins

--- dokuwiki/conf/plugins.local.php
+++ dokuwiki/conf/plugins.local.php
@@ -6,3 +6,15 @@
  * NOTE: Plugins will not be added to this file unless there is a need to override a default setting. Plugins are
  *       enabled by default, unless having a 'disabled' file in their plugin folder.
  */
+/*
+$plugins['authad'] = 0;
+$plugins['authldap'] = 0;
+$plugins['authmysql'] = 0;
+$plugins['authpgsql'] = 0;
+$plugins['authplain'] = 0;
+$plugins['comment'] = 0;
+$plugins['hcalendar'] = 0;
+$plugins['include'] = 0;
+$plugins['recaptcha'] = 0;
+$plugins['vcard'] = 0;
+ */

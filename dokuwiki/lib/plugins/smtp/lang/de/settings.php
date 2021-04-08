d97329f47f507e79239d16611046d2e97ca1746c
Niklas Keller <me@kelunik.com>
2021-04-08 14:30:00 +0200

    Upgrade smtp plugin

--- dokuwiki/lib/plugins/smtp/lang/de/settings.php
+++ dokuwiki/lib/plugins/smtp/lang/de/settings.php
@@ -3,15 +3,16 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Andreas Gohr <andi@splitbrain.org>
  * @author Dominik Eckelmann <deckelmann@gmail.com>
  */
-$lang['smtp_host']             = 'Dein ausgehender SMTP Server';
-$lang['smtp_port']             = 'Der Port deines SMTP Servers (Standard 25 oder 465 für SSL)';
-$lang['smtp_ssl']              = 'Welche Verschlüsslung wird bei der Kommunikation mit deinem SMTP Server verwendet.';
-$lang['smtp_ssl_o_']          = 'Keine';
-$lang['smtp_ssl_o_ssl']          = 'SSL';
-$lang['smtp_ssl_o_tls']          = 'TLS';
-$lang['auth_user']             = 'Trage hier den Benutzernamen ein, falls eine Anmeldung am STMP Server erforderlich ist.';
-$lang['auth_pass']             = 'Passwort für den Benutzer darüber.';
-$lang['localdomain']           = 'Füge hier den namen, welcher während des HELO Aufrufes von SMTP verwendet werden soll. Dies sollte der FQDN deines DokuWiki Webservers sein. Wenn du es Leer lässt wird DokuWiki versuchen es selbst zu ermitteln.';
-$lang['debug']                 = 'Kompletten Fehlerbericht ausgeben wenn das Versenden fehlschlägt. Deaktiviere es wenn das Versenden Funktioniert.';
+$lang['smtp_host']             = 'Ihr ausgehender SMTP Server';
+$lang['smtp_port']             = 'Der Port Ihres SMTP Servers (Standard 25 oder 465 für SSL)';
+$lang['smtp_ssl']              = 'Welche Verschlüsslung wird bei der Kommunikation mit dem SMTP Server verwendet?';
+$lang['smtp_ssl_o_']           = 'Keine';
+$lang['smtp_ssl_o_ssl']        = 'SSL';
+$lang['smtp_ssl_o_tls']        = 'TLS';
+$lang['auth_user']             = 'Tragen Sie hier den Benutzernamen ein, falls eine Anmeldung am SMTP Server erforderlich ist.';
+$lang['auth_pass']             = 'Passwort für den obenstehenden Benutzer.';
+$lang['localdomain']           = 'Fügen Sie hier den Namen ein, welcher während des HELO Aufrufes von SMTP verwendet werden soll. Dies sollte der FQDN Ihres DokuWiki Webservers sein. Wenn das Feld leer gelassen wird, wird DokuWiki versuchen es selbst zu ermitteln.';
+$lang['debug']                 = 'Kompletten Fehlerbericht ausgeben wenn das Versenden fehlschlägt. Deaktiviere sobald das Versenden funktioniert!';
---
+++ dokuwiki/lib/plugins/smtp/lang/es/lang.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Alicia Esponda <aesponda@ieee.org>
+ */
+$lang['menu']                  = 'Verifica la configuración SMTP';
+$lang['nofrom']                = 'No configuraste la opción "correo de envío".  Probablemente fallará el envío de correos electrónicos.';
